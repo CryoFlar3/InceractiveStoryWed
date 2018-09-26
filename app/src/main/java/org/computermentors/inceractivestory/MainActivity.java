d7bcae4dcabed6b9f29c8395a9cfcadf9eccee5e
CryoFlar3 <ndukes@computermentors.org>
2018-09-26 19:53:09 -0400

    Created a second activity and linked the activities together.

--- app/src/main/java/org/computermentors/inceractivestory/MainActivity.java
+++ app/src/main/java/org/computermentors/inceractivestory/MainActivity.java
@@ -1,13 +1,39 @@
 package org.computermentors.inceractivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameEditText = findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEditText.getText().toString().trim();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
     }
 }
